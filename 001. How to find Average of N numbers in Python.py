3874ccf64bc16408b11f10cb117189b698375033
StarkEdward <skamble454@gmail.com>
2022-06-05 10:32:50 +0530

    001

---
+++ 001. How to find Average of N numbers in Python.py
@@ -0,0 +1,12 @@
+# How to find average of N numbers in Python.
+
+num = int(input("How many numbers? :"))
+total_sum = 0
+
+for n in range(num):
+    numbers = float(input(f"Enter {n+1} number : "))
+    total_sum += numbers
+
+avg = round(total_sum / num, 2)
+
+print(f"Average is: {avg}")

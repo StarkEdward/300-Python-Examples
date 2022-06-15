97abc37330b8d4590d6106b62906251bf98a2c92
StarkEdward <skamble454@gmail.com>
2022-06-15 21:40:22 +0530

    030 How to find area of triangle given base and height

---
+++ 030. How to find area of triangle given base and height..py
@@ -0,0 +1,8 @@
+# 030 How to find area of triangle given base and height
+
+b = float(input("Enter the base of triangle: "))
+h = float(input("Enter the height of triangle: "))
+
+area = round((b * h) / 2 , 2)
+
+print(f"Thea are of triangle is: {area}")

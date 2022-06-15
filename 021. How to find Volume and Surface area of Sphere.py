6290b8a5c18fa6e350fada878b7af8803cd3a825
StarkEdward <skamble454@gmail.com>
2022-06-15 20:39:42 +0530

    021 how to find volume and surface are of sphere

---
+++ 021. How to find Volume and Surface area of Sphere.py
@@ -0,0 +1,6 @@
+# how to find volume and surface are of sphere
+from math import pi
+r = int(input("Enter the radius of Sphere: "))
+v = 4 / 3 * pi * r * 3
+
+print(f"The Volume of the sphere is: {v}")

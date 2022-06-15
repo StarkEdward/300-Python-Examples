ef160d245dbcc92d9e46b907851fc1c503b6f499
StarkEdward <skamble454@gmail.com>
2022-06-15 21:12:06 +0530

    025. How to find whether an integer is even or odd number

---
+++ 025. How to find whether an integer is even or odd number.py
@@ -0,0 +1,8 @@
+# 025. How to find whether an integer is even or odd number
+
+num = int(input("Enter number to check even or odd: "))
+
+if num % 2 == 0:
+    print(f"The {num} is even number.")
+else:
+    print(f"The {num} is odd number.")

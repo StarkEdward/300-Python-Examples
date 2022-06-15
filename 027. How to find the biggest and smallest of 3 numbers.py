4b5be715e321cc1d89c8e0afd346e7561e55fc7e
StarkEdward <skamble454@gmail.com>
2022-06-15 21:21:54 +0530

    027. How to find the biggest and smallest of 3 numbers

---
+++ 027. How to find the biggest and smallest of 3 numbers.py
@@ -0,0 +1,10 @@
+# 027. How to find the biggest and smallest of 3 numbers
+
+num1 = 3
+num2 = 6
+num3 = 66
+
+list = [num1, num2, num3]
+
+print(f"the biggest number is: {max(list)}")
+print(f"the smallest number is: {min(list)}")
